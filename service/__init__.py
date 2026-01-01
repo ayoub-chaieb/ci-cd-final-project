ee1ebc0160bc3011b3afaad5bcd54e0c0d8e1dbd
Ayoub <ayoubchaieb75@gmail.com>
2026-01-01 09:47:01 -0500

    Add OpenTelemetry tracing to Flask service

--- service/__init__.py
+++ service/__init__.py
@@ -2,8 +2,39 @@
 Service Package
 """
 from flask import Flask
+from opentelemetry import trace
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 
 app = Flask(__name__)
+
+# -------------------------------
+# OpenTelemetry initialization
+# -------------------------------
+resource = Resource.create({
+    "service.name": os.getenv("OTEL_SERVICE_NAME", "cicd-app")
+})
+
+trace.set_tracer_provider(TracerProvider(resource=resource))
+
+otlp_endpoint = os.getenv(
+    "OTEL_EXPORTER_OTLP_ENDPOINT",
+    "otel-collector:4317"
+)
+
+otlp_exporter = OTLPSpanExporter(
+    endpoint=otlp_endpoint,
+    insecure=True  # OK for lab/sandbox
+)
+
+span_processor = BatchSpanProcessor(otlp_exporter)
+trace.get_tracer_provider().add_span_processor(span_processor)
+
+# Instrument Flask AFTER app creation
+FlaskInstrumentor().instrument_app(app)
 
 # This must be imported after the Flask app is created
 from service import routes               # pylint: disable=wrong-import-position,cyclic-import
